ec1feaba54da354657bd348b66664ae967bb4566
Grimels <grimels.ua@gmail.com>
2018-10-09 00:41:44 +0300

    Task 1. Added ucFirst() function.

--- task1.js
+++ task1.js
@@ -7,4 +7,10 @@ function reverse(str){
     return res;
 }
 
+function ucFirst(str){
+    return str.length > 0 ? `${str[0].toUpperCase()}${str.substring(1, str.length)}` : '';
+}
+
 console.log(`reverse('Hello World!') => ${reverse('Hello World!')}`);
+console.log(`ucFirst('tomorrow never comes') => ${ucFirst('tomorrow never comes')}`);
+
